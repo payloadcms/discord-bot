31528afc6859454578ff51436b16f0967743d3b9
Alessio Gravili <alessio@gravili.de>
2024-04-17 11:02:36 -0400

    feat: lock job posts if they don't follow the rules

--- src/listeners/job-created.ts
+++ src/listeners/job-created.ts
@@ -1,13 +1,24 @@
-import { Client } from 'discord.js';
+import { Client, ThreadChannel } from 'discord.js';
 import { isJobThread } from '../helpers/is-job';
 
 export default (client: Client): void => {
   client.on('threadCreate', async (thread, newlyCreated) => {
     if (newlyCreated && isJobThread(thread)) {
-      const channel = await client.channels.fetch(thread.id);
+      const channel: ThreadChannel = await client.channels.fetch(thread.id) as ThreadChannel;
       if (channel !== null && channel.isTextBased()) {
-        await channel.send(`A new forum post has been created: ${thread.name}`);
+        if(!channel.name || (!channel.name.startsWith('[HIRING]') && !channel.name.startsWith('[SEEKING WORK]'))) {
+
+          await channel.send('This job post does not follow the channel rules. Please delete this post, read the channel rules (first post in the channel) and create a new one.')
+
+
+
+
+          await channel.setLocked(true, 'Does not follow channel rules')
+          await channel.setArchived(true, 'Does not follow channel rules')
+
+        }
       }
     }
   });
 };
+
